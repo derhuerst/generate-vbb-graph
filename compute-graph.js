f62d6f782088e6a3efe4154ca1e2bb9ad791f899
Jannis R <mail@jannisr.de>
2018-05-03 02:26:38 +0200

    refactor: use writable streams

--- compute-graph.js
+++ compute-graph.js
@@ -2,11 +2,13 @@
 
 const mapping = require('merged-vbb-stations')
 const deepEqual = require('lodash.isequal')
+const pump = require('pump')
 const readLines = require('vbb-lines')
+const {Writable} = require('stream')
+const readSchedules = require('vbb-trips').schedules
 const shorten = require('vbb-short-station-name')
 
 const allStations = require('./lib/merged-stations')
-const readSchedules = require('./lib/read-schedules')
 
 // stop -> station mapping
 const stationOf = Object.create(null)
@@ -49,9 +51,18 @@ const computeGraph = (nodes, edges, cb, opt = {}) => {
 	const wroteNode = {} // by ID
 	const wroteEdge = {} // by source ID + target ID + relation + metadata
 
+	const lines = Object.create(null) // by id
+	const writeLine = (line, _, cb) => {
+		if (filterLines(line)) {
+			lines[line.id] = Object.assign({}, line, {
+				variants: deduplicateVariants(line)
+			})
+		}
+		cb()
+	}
+
 	const writeStation = (id) => {
 		const s = allStations[id]
-		if (!s) return // todo: log err
 		wroteNode[id] = true
 		const node = {id: s.id, label: shorten(s.name)}
 
@@ -69,62 +80,75 @@ const computeGraph = (nodes, edges, cb, opt = {}) => {
 		nodes.write(node)
 	}
 
-	const lines = Object.create(null) // by id
-	readLines()
-	.on('error', cb)
-	.on('data', (line) => {
-		if (!filterLines(line)) return
+	const writeSchedule = (s, _, cb) => {
+		const line = lines[s.route.line]
+		if (!line) return cb()
 
-		lines[line.id] = Object.assign({}, line, {
-			variants: deduplicateVariants(line)
-		})
-	})
-	.on('end', () => {
-		readSchedules(lines)
-		.on('error', cb)
-		.on('data', (s) => {
-			const line = s.route.line
-			// check if this variant appears in the filtered list of line variants
-			if (!line.variants.some(isEqualVariant(s.route.stops))) return
+		// check if this variant appears in the filtered list of line variants
+		if (!line.variants.some(isEqualVariant(s.route.stops))) return cb()
 
-			for (let i = 0; (i + 1) < s.route.stops.length; i++) {
-				const current = stationOf[s.route.stops[i]]
-				if (!filterStations(current)) return false
-				const next = stationOf[s.route.stops[i + 1]]
-
-				if (!wroteNode[current]) writeStation(current)
-				if (!wroteNode[next]) writeStation(next)
-
-				let start = s.sequence[i]
-				if ('number' === typeof start.departure)  start = start.departure
-				else if ('number' === typeof start.arrival) start = start.arrival
-				else continue // todo
-
-				let end = s.sequence[i + 1]
-				if ('number' === typeof end.departure)  end = end.departure
-				else if ('number' === typeof end.arrival) end = end.arrival
-				else continue // todo
-
-				let signature = [
-					current, next,
-					line.product, line.name
-				]
-				if (!opt.simpleDeduplication) signature.push(end - start)
-				signature = signature.join('-')
-
-				if (!wroteEdge[signature]) {
-					wroteEdge[signature] = true
-					edges.write({
-						source: current,
-						target: next,
-						relation: line.product,
-						metadata: {line: line.name, time: end - start}
-					})
-				}
+		for (let i = 0; (i + 1) < s.route.stops.length; i++) {
+			const current = stationOf[s.route.stops[i]]
+			if (!filterStations(current)) continue
+			if (!allStations[current]) {
+				return cb(new Error('unknown station ' + current))
 			}
-		})
-		.on('end', cb)
-	})
+			const next = stationOf[s.route.stops[i + 1]]
+			// todo: filterStations(next) ?
+			if (!allStations[next]) {
+				return cb(new Error('unknown station ' + next))
+			}
+
+			if (!wroteNode[current]) writeStation(current)
+			if (!wroteNode[next]) writeStation(next)
+
+			let start = s.sequence[i]
+			if ('number' === typeof start.departure)  start = start.departure
+			else if ('number' === typeof start.arrival) start = start.arrival
+			else continue // todo
+
+			let end = s.sequence[i + 1]
+			if ('number' === typeof end.departure)  end = end.departure
+			else if ('number' === typeof end.arrival) end = end.arrival
+			else continue // todo
+
+			let signature = [
+				current, next,
+				line.product, line.name
+			]
+			if (!opt.simpleDeduplication) signature.push(end - start)
+			signature = signature.join('-')
+
+			if (!wroteEdge[signature]) {
+				wroteEdge[signature] = true
+				edges.write({
+					source: current,
+					target: next,
+					relation: line.product,
+					metadata: {line: line.name, time: end - start}
+				})
+			}
+		}
+		cb()
+	}
+
+	pump(
+		readLines(),
+		new Writable({objectMode: true, write: writeLine}),
+		(err) => {
+			if (err) return cb(err)
+			pump(
+				readSchedules(),
+				new Writable({objectMode: true, write: writeSchedule}),
+				(err) => {
+					if (err) return cb(err)
+					nodes.end()
+					edges.end()
+					cb()
+				}
+			)
+		}
+	)
 }
 
 module.exports = computeGraph
